aae225ed7e1879c1fb6b4dce342cefb4dcf8d482
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-09-26 14:02:49 +0200

    ola

---
+++ src/Ejercicio1.java
@@ -0,0 +1,9 @@
+public class Ejercicio1 {
+    public static void main(@org.jetbrains.annotations.NotNull String[] args) {
+        if(args.length <1) {
+            System.exit(1);
+        } else if (args[0].) {
+            
+        }
+    }
+}
